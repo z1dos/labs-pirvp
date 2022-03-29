38ca37d4ca3e13422793b0e543356cce9b4f06f9
z1dos <georg.awdeew2017@yandex.ru>
2022-03-29 22:36:02 +0700

    задание 6.2 и 6.3

--- prac_6/prac_lab_6_1.php
+++ prac_6/prac_lab_6_1.php
@@ -15,30 +15,40 @@ if ($_POST) {
     echo '<p>Время:' . date('l jS \of F Y H:i:s A') . '</p>';
     echo '<p>Время:' . date('H') . '</p>';
 
-    if (5 <= date('H') and date('H') < 11){
+    if (5 <= date('H') and date('H') < 11) {
         echo '<p>Доброе утро, ' . $nameSurname . '</p>';
+        echo '<p>Сведения о вас:' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
     }
 
-    if (11 <= date('H') and date('H') < 16){
+    if (11 <= date('H') and date('H') < 16) {
         echo '<p>Добрый день, ' . $nameSurname . '</p>';
+        echo '<p>Сведения о вас:' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
     }
 
-    if (16 <= date('H') and date('H') < 24){
+    if (16 <= date('H') and date('H') < 24) {
         echo '<p>Добрый вечер, ' . $nameSurname . '</p>';
+        echo '<p>Сведения о вас:' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
     }
 
-    if (0 <= date('H') and date('H') < 5){
+    if (0 <= date('H') and date('H') < 5) {
         echo '<p>Доброй ночи, ' . $nameSurname . '</p>';
+        echo '<p>Сведения о вас:' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
     }
 }
 ?>
-<p>Задание №6.1</p>
-<form method="post">
-    <label for="name">Введите имя</label><br>
-    <input name="name"><br>
-    <label for="surname">Введите фамилию</label><br>
-    <input name="surname"><br>
-    <input type="submit" value="Отправить">
-</form>
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
+    echo "<h1>Вы не можете пользоваться сайтом с этого браузера!</h1>";
+} else {
+    echo "<p>Задание №6.1</p>
+    <form method='post'>
+    <label for='name'>Введите имя</label><br>
+    <input name='name'><br>
+    <label for='surname'>Введите фамилию</label><br>
+    <input name='surname'><br>
+    <input type='submit' value='Отправить'>
+</form>";
+}
+?>
 </body>
 </html>
