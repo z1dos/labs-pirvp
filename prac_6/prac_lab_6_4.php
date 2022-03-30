8e4c589cf0b042e6bd86f748045132e5ced89a25
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-03-30 15:18:05 +0700

    Корректировка

--- prac_6/prac_lab_6_4.php
+++ prac_6/prac_lab_6_4.php
@@ -1,12 +1,11 @@
 <?php
 $file = file('ips.txt');
 $hostname = $_SERVER['REMOTE_ADDR'];
+$count = implode("", $file);
+$count++;
 
 if (in_array($hostname, $file)){
-    $count = implode("", $file);
-    $count++;
-
-    file_put_contents('ips.txt', $count, FILE_APPEND);
+    echo 'Вы уже посещали эту страницу';
 } else {
     file_put_contents('ips.txt', $hostname, FILE_APPEND);
 }
