72d1280ceb0a00f72892e4a829db1ac4278f9eff
z1dos <gr403_avgan@ttit.local>
2022-05-21 14:38:19 +0700

    prov_3

--- prov_3/find.php
+++ prov_3/find.php
@@ -1,9 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>todo list</title>
+</head>
+<body>
+<h2>Поиск задачи</h2>
+<form action="find.php" method="GET">
+    <input type="text" name="title">
+    <input type="submit" value="Найти">
+</form>
+<h2>Задачи по запросу</h2>
+<h3>Невыполненные</h3>
+<ul>
+    <?php
 
-require 'db.php';
+    require 'db.php';
 
-$title = $_GET['title'];
+    $title = $_GET['title'];
 
-$sql = "SELECT * FROM `list` WHERE `title` = ?";
-$query = $pdo->prepare($sql);
-$query->execute([$title]);
+    $sql = "SELECT * FROM `list` WHERE `title` = ?";
+    $query = $pdo->prepare($sql);
+    $query = $pdo->query("SELECT * FROM `list` WHERE `title` = '$title' AND `done` = 'no' ");
+
+    while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+        echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . '<a href="done.php?id=' . $row->id . '">Выполенено</a>' . ' </div>';
+    }
+    ?>
+</ul>
+<h3>Выполенные</h3>
+<ul>
+    <?php
+    require 'db.php';
+
+    $title = $_GET['title'];
+
+    $sql = "SELECT * FROM `list` WHERE `title` = ?";
+    $query = $pdo->prepare($sql);
+    $query = $pdo->query("SELECT * FROM `list` WHERE `title` = '$title' AND `done` = 'done' ");
+
+    while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+        echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . ' </div>';
+    }
+    ?>
+</ul>
+<h2><a href="index.php">Вернуться к добавлению</a> </h2>
+</body>
+</html>
--- prov_3/index.php
+++ prov_3/index.php
@@ -6,31 +6,15 @@
     <title>todo list</title>
 </head>
 <body>
-<!--<h2>Поиск задачи</h2>-->
-<!--<form action="find.php" method="GET">-->
-<!--    <input type="text" name="title">-->
-<!--    <input type="submit" value="Add">-->
-<!--</form>-->
-<!--<ul>-->
-<!--    --><?php
-//    require 'db.php';
-//
-//    $query = $pdo->query("SELECT * FROM `list` ORDER BY `id` DESC");
-//
-//    while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-//        echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . '<a href="done.php?id=' . $row->id . '">done</a>' . ' </div>';
-//    }
-//    ?>
-<!--</ul>-->
 <h2>Поиск задачи</h2>
 <form action="find.php" method="GET">
     <input type="text" name="title">
-    <input type="submit" value="Add">
+    <input type="submit" value="Найти">
 </form>
 <h2>Добавить задачу</h2>
 <form action="add.php" method="POST">
     <input type="text" name="title" required>
-    <input type="submit" value="Add">
+    <input type="submit" value="Добавить">
 </form>
 <h2>Ваши задачи</h2>
 <ul>
@@ -41,7 +25,7 @@
     $query = $pdo->query("SELECT * FROM `list` where `done` = 'no' ORDER BY `id` DESC");
 
     while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-        echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . '<a href="done.php?id=' . $row->id . '">done</a>' . ' </div>';
+        echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . '<a href="done.php?id=' . $row->id . '">Выполенено</a>' . ' </div>';
     }
     ?>
 </ul>
@@ -50,7 +34,7 @@
     <?php
     require 'db.php';
 
-    $query = $pdo->query("SELECT * FROM `list` where `done` = 'done'");
+    $query = $pdo->query("SELECT * FROM `list` where `done` = 'done' ORDER BY `id` DESC");
 
     while ($row = $query->fetch(PDO::FETCH_OBJ)) {
         echo '<div><li>' . $row->title . ' <a href="delete.php?id=' . $row->id . '">X</a></li>' . ' </div>';
